6c54b2f2d0c79a0400786114d1a6f7e01213da85
Swanith-AI29 <shobhasrikanth1319@gmail.com>
2025-05-27 11:29:42 +0530

    Variables Names

---
+++ Getting Started with Programming/Keywords.py
@@ -0,0 +1,6 @@
+number=5
+y="Swanith"
+print(number)
+print(y)
+n=input("enter your name-")
+print(n)
--- Getting Started with Programming/Main.py
+++ Getting Started with Programming/Main.py
@@ -4,4 +4,5 @@ print('My name is Swanith')
 print("My name isn't Swanith")
 print("Add12+9=",12+9)
 print("Python",end="Programming")
+print("High\n","level")
 
