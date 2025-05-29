c4b5d2bb54f96845a39060be1ddd5f8bcdb0230d
Swanith-AI29 <shobhasrikanth1319@gmail.com>
2025-05-29 22:41:57 +0530

    Commit

---
+++ Data Types/Project.py
@@ -0,0 +1,17 @@
+name1="Arjun"
+birthday1="10-1-2010"
+name2="Meera"
+birthday2="23-4-2009"
+name3="Rahul"
+birthday3="15-8-2011"
+name4="Sneha"
+birthday4="01-12-2008"
+name5="Dev"
+birthday5="30-06-2012"
+print(name1,"birthday is on",birthday1)
+print(name2,"birthday is on",birthday2)
+print(name3,"birthday is on",birthday3)
+print(name4,"birthday is on",birthday4)
+print(name5,"birthday is on",birthday5)
+
+
