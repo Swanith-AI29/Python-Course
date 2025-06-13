ff30a81a864d700c3ba6fe2e89b1e63ed4aeb201
Swanith-AI29 <shobhasrikanth1319@gmail.com>
2025-06-13 18:20:28 +0530

    Commit

--- Extra Projects/Alphabets.py
+++ Extra Projects/Alphabets.py
@@ -1,6 +1,7 @@
-Alp=int(input("Enter the letter"))
-if Alp A< and >Z :
+Alp=str(input("Enter the letter"))
+if Alp>="A" and Alp<="Z":
     print("It is an alphabet")
+elif Alp>="a" and Alp<="z":
+    print("It is an alphabet")    
 else:
     print("It is non alphabet")
-
