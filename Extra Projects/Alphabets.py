a02856f4e1ab9d0356bccdb9e82ccdbd15f24cc1
Swanith-AI29 <shobhasrikanth1319@gmail.com>
2025-06-13 16:45:37 +0530

    Commit

---
+++ Extra Projects/Alphabets.py
@@ -0,0 +1,6 @@
+Alp=int(input("Enter the letter"))
+if Alp A< and >Z :
+    print("It is an alphabet")
+else:
+    print("It is non alphabet")
+
---
+++ Extra Projects/Summertimes.py
@@ -0,0 +1,5 @@
+Weather=int(input("Enter the temperture in your sorroundings-"))
+if Weather>30:
+    print("It is summer so wear light clothes")
+else:
+    print("It is winter so wear warm clothes")
