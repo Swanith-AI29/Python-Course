b2bf0c7b62c96a275b808744d909d46d30b98deb
Swanith-AI29 <shobhasrikanth1319@gmail.com>
2025-07-25 19:32:28 +0530

    commit

---
+++ Homework/Patterns.py
@@ -0,0 +1,12 @@
+line=int(input("How many lines do you want?"))
+for i in range(0,line):
+    for j in range (0,line):
+        print("*",end="")
+    print()
+
+
+
+for i in range(0,line):
+    for j in range(0,i+1):
+        print("$",end=" ")
+    print()
