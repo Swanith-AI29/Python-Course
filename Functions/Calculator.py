da8d18d9cf5cc81ce8600057485d5922bf9e509f
Swanith-AI29 <shobhasrikanth1319@gmail.com>
2025-08-02 14:48:41 +0530

    commit

--- Functions/Calculator.py
+++ Functions/Calculator.py
@@ -1,5 +1,11 @@
 def Add(num1,num2):
     return num1+num2
+def Subtraction(num1,num2):
+    return num2-num1
+def Multiply(num1,num2):
+    return num1*num2
+def Division(num1,num2):
+    return num2/num1
 print("Pick your operation-")
 print("1. Add")
 print("2. Subtract")
@@ -10,3 +16,11 @@ num1=int(input("Enter first number-"))
 num2=int(input("Enter your second number-"))
 if Choice=="1":
     print("Answer of addition-",Add(num1,num2))
+elif Choice=="2":
+    print("Answer of subtraction-",Subtraction(num1,num2))
+elif Choice=="3":
+    print("Answer of Multiplication-",Multiply(num1,num2))
+elif Choice=="4":
+    print("Answer of division-",Division(num1,num2))
+else:
+    print("Invalid input")
---
+++ Functions/Functionswitharguments.py
@@ -0,0 +1,6 @@
+def Greetings(name,Message="Hi "):
+    return f"{Message}{name}"
+print (Greetings("swanith"))
+def Details(Name,Subject,Grail="9th"):
+    return f"{Name}{Subject}{Grail}"
+print (Details("Swanith","Maths"))
