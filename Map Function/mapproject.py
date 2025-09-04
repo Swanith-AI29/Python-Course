6fb56b0633346c313ea3e581ca9871b7a382131c
Swanith-AI29 <shobhasrikanth1319@gmail.com>
2025-09-04 21:05:36 +0530

    commit

---
+++ Map Function/mapproject.py
@@ -0,0 +1,17 @@
+numbers=[5,6,7,8,9]
+def add(n):
+    return n+n 
+add=list(map(add,numbers))
+print(add)
+def subtract(n):
+    return n-n 
+subtract=list(map(subtract,numbers))
+print(subtract)
+def square(n):
+    return n*n 
+square=list(map(square,numbers))
+print(square)
+def divide(n):
+    return n/n 
+divide=list(map(divide,numbers))
+print(divide)
