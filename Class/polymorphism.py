ea969ee1922c2535207b923aec10b1e67b0b022d
Swanith-AI29 <shobhasrikanth1319@gmail.com>
2025-10-24 18:41:27 +0530

    commit

---
+++ Class/polymorphism.py
@@ -0,0 +1,11 @@
+class hw():
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def add(self):
+        add=self.x+self.y
+        print(add)
+
+answer=hw(9,20)
+answer.add()
+        
